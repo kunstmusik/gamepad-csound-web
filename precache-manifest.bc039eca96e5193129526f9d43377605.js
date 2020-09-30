8d5355b85d49390bae80592649796abae1b6c9b8
kunstmusik <kunstmusik@users.noreply.github.com>
2020-09-30 23:58:46 +0000

    deploy: 7902780e72ef875cfda3802f3f0cbd3b689e2c2e

---
+++ precache-manifest.bc039eca96e5193129526f9d43377605.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6331c09e49fe0bccca6be2006a21a3af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf1642175c841e3b22c0",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ac921bc2bb5f3c24dadd",
+    "url": "/static/js/2.c38f5fed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c38f5fed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf1642175c841e3b22c0",
+    "url": "/static/js/main.71f88d44.chunk.js"
+  },
+  {
+    "revision": "9ed3f7a7a73cb3cdb1dd",
+    "url": "/static/js/runtime-main.e42753dd.js"
+  }
+]);

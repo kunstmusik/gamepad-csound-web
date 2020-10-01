97557caacb7f1e25e0a3e774c675429262f5b2be
kunstmusik <kunstmusik@users.noreply.github.com>
2020-10-01 01:17:47 +0000

    deploy: 9837930b51cac1aaf3cfe66a8cbb0200cb82834c

--- precache-manifest.1984ab7ef5c1df6c41673a8e78deafa1.js
+++ precache-manifest.1984ab7ef5c1df6c41673a8e78deafa1.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3791bd2f71a64220f976764c35e8cb43",
+    "revision": "ffe74f5a7ee66adaa5194bf5f2f4f3a3",
     "url": "./index.html"
   },
   {
-    "revision": "dd6dcd03a45310d41c56",
+    "revision": "86b5b38554d91e312812",
     "url": "./static/css/main.b1e51f07.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/js/2.c38f5fed.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "dd6dcd03a45310d41c56",
-    "url": "./static/js/main.cc88bfc8.chunk.js"
+    "revision": "86b5b38554d91e312812",
+    "url": "./static/js/main.9c397eb1.chunk.js"
   },
   {
     "revision": "c4e8440ee6dcef2b1341",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.18cbf4d65292f3689a57df38cd398a08.js"
+  "./precache-manifest.1984ab7ef5c1df6c41673a8e78deafa1.js"
 );
 
 self.addEventListener('message', (event) => {
